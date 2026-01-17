225138fd33875aa22f13387079bd86b0559206fc
Luciano 16-gif <chanominardo10@gmail.com>
2026-01-16 22:15:55 -0400

    first commit

---
+++ src/main/java/com/mycompany/project1os/Project1OS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1os;
+
+/**
+ *
+ * @author chano
+ */
+public class Project1OS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
